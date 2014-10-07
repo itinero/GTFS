172a4d8a2497b731b9a2271dab0e6a62d1c32e79
xivk <ben@osmsharp.com>
2014-10-07 17:18:12 +0200

    Added a feed routes filter.

--- GTFS/Filters/GTFSFeedRoutesFilter.cs
+++ GTFS/Filters/GTFSFeedRoutesFilter.cs
@@ -27,32 +27,32 @@ using System.Collections.Generic;
 namespace GTFS.Filters
 {
     /// <summary>
-    /// Represents a GTFS feed filter that leaves only date related to the given stops.
+    /// Represents a GTFS feed filter that leaves only data related to the given routes.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class GTFSFeedStopsFilter : GTFSFeedFilter
+    public class GTFSFeedRoutesFilter : GTFSFeedFilter
     {
         /// <summary>
-        /// Holds the stop filter function.
+        /// Holds the routes filter function.
         /// </summary>
-        private Func<Stop, bool> _stopFilter;
+        private Func<Route, bool> _routesFilter;
 
         /// <summary>
-        /// Creates a new stops filter.
+        /// Creates a new routes filter.
         /// </summary>
-        /// <param name="stopFilter"></param>
-        public GTFSFeedStopsFilter(Func<Stop, bool> stopFilter)
+        /// <param name="routesFilter"></param>
+        public GTFSFeedRoutesFilter(Func<Route, bool> routesFilter)
         {
-            _stopFilter = stopFilter;
+            _routesFilter = routesFilter;
         }
 
         /// <summary>
-        /// Creates a new stops filter.
+        /// Creates a new routes filter.
         /// </summary>
-        /// <param name="stopIds"></param>
-        public GTFSFeedStopsFilter(HashSet<string> stopIds)
+        /// <param name="routeIds"></param>
+        public GTFSFeedRoutesFilter(HashSet<string> routeIds)
         {
-            _stopFilter = (s) => stopIds.Contains(s.Id);
+            _routesFilter = (r) => routeIds.Contains(r.Id);
         }
 
         /// <summary>
@@ -64,55 +64,12 @@ namespace GTFS.Filters
         {
             var filteredFeed = new GTFSFeed();
 
-            // filter stops.
-            var stopIds = new HashSet<string>();
-            foreach (var stop in feed.GetStops())
-            {
-                if(_stopFilter.Invoke(stop))
-                { // stop has to be included.
-                    stopIds.Add(stop.Id);
-                    filteredFeed.AddStop(stop);
-                }
-            }
-
-            // filter stop-times.
-            var tripIds = new HashSet<string>();
-            foreach(var stopTime in feed.GetStopTimes())
-            {
-                if(stopIds.Contains(stopTime.StopId))
-                { // stop is included, keep this stopTime.
-                    filteredFeed.AddStopTime(stopTime);
-
-                    // save the trip id's to keep.
-                    tripIds.Add(stopTime.TripId);
-                }
-            }
-            
-            // filter trips.
-            var routeIds = new HashSet<string>();
-            var serviceIds = new HashSet<string>();
-            var shapeIds = new HashSet<string>();
-            foreach(var trip in feed.GetTrips())
-            {
-                if(tripIds.Contains(trip.Id))
-                { // keep this trip, it is related to at least one stop-time.
-                    filteredFeed.AddTrip(trip);
-
-                    // keep serviceId, routeId and shapeId.
-                    routeIds.Add(trip.RouteId);
-                    serviceIds.Add(trip.ServiceId);
-                    if(!string.IsNullOrWhiteSpace(trip.ShapeId))
-                    {
-                        shapeIds.Add(trip.ShapeId);
-                    }
-                }
-            }
-            
             // filter routes.
+            var routeIds = new HashSet<string>();
             var agencyIds = new HashSet<string>();
-            foreach(var route in feed.GetRoutes())
+            foreach (var route in feed.GetRoutes())
             {
-                if(routeIds.Contains(route.Id))
+                if (_routesFilter.Invoke(route))
                 { // keep this route.
                     filteredFeed.AddRoute(route);
 
@@ -121,28 +78,71 @@ namespace GTFS.Filters
                 }
             }
 
-            // filter agencies.
-            foreach(var agency in feed.GetAgencies())
+            // filter trips.
+            var serviceIds = new HashSet<string>();
+            var shapeIds = new HashSet<string>();
+            foreach (var trip in feed.GetTrips())
             {
-                if(agencyIds.Contains(agency.Id))
+                if (routeIds.Contains(trip.RouteId))
+                { // keep this trip, it is related to at least one stop-time.
+                    filteredFeed.AddTrip(trip);
+
+                    // keep serviceId, routeId and shapeId.
+                    serviceIds.Add(trip.ServiceId);
+                    if (!string.IsNullOrWhiteSpace(trip.ShapeId))
+                    {
+                        shapeIds.Add(trip.ShapeId);
+                    }
+                }
+            }
+
+            // filter stop-times.
+            var tripIds = new HashSet<string>();
+            var stopIds = new HashSet<string>();
+            foreach (var stopTime in feed.GetStopTimes())
+            {
+                if (tripIds.Contains(stopTime.TripId))
+                { // stop is included, keep this stopTime.
+                    filteredFeed.AddStopTime(stopTime);
+
+                    // save the trip id's to keep.
+                    stopIds.Add(stopTime.StopId);
+                }
+            }
+
+
+            // filter stops.
+            foreach (var stop in feed.GetStops())
+            {
+                if (stopIds.Contains(stop.Id))
+                { // stop has to be included.
+                    stopIds.Add(stop.Id);
+                    filteredFeed.AddStop(stop);
+                }
+            }
+
+            // filter agencies.
+            foreach (var agency in feed.GetAgencies())
+            {
+                if (agencyIds.Contains(agency.Id))
                 { // keep this agency.
                     filteredFeed.AddAgency(agency);
                 }
             }
 
             // filter calendars.
-            foreach(var calendar in feed.GetCalendars())
+            foreach (var calendar in feed.GetCalendars())
             {
-                if(serviceIds.Contains(calendar.ServiceId))
+                if (serviceIds.Contains(calendar.ServiceId))
                 { // keep this calendar.                    
                     filteredFeed.AddCalendar(calendar);
                 }
             }
 
             // filter calendar-dates.
-            foreach(var calendarDate in feed.GetCalendarDates())
+            foreach (var calendarDate in feed.GetCalendarDates())
             {
-                if(serviceIds.Contains(calendarDate.ServiceId))
+                if (serviceIds.Contains(calendarDate.ServiceId))
                 { // keep this calendarDate.                    
                     filteredFeed.AddCalendarDate(calendarDate);
                 }
@@ -150,9 +150,9 @@ namespace GTFS.Filters
 
             // filter fare rules.
             var fareIds = new HashSet<string>();
-            foreach(var fareRule in feed.GetFareRules())
+            foreach (var fareRule in feed.GetFareRules())
             {
-                if(routeIds.Contains(fareRule.RouteId))
+                if (routeIds.Contains(fareRule.RouteId))
                 { // keep this fare rule.
                     filteredFeed.AddFareRule(fareRule);
 
@@ -160,28 +160,28 @@ namespace GTFS.Filters
                     fareIds.Add(fareRule.FareId);
                 }
             }
-            
+
             // filter fare attributes.
-            foreach(var fareAttribute in feed.GetFareAttributes())
+            foreach (var fareAttribute in feed.GetFareAttributes())
             {
-                if(fareIds.Contains(fareAttribute.FareId))
+                if (fareIds.Contains(fareAttribute.FareId))
                 { // keep this fare attribute.
                     filteredFeed.AddFareAttribute(fareAttribute);
                 }
             }
 
             // filter frequencies.
-            foreach(var frequency in feed.GetFrequencies())
+            foreach (var frequency in feed.GetFrequencies())
             {
-                if(tripIds.Contains(frequency.TripId))
+                if (tripIds.Contains(frequency.TripId))
                 { // keep this frequency.
                     filteredFeed.AddFrequency(frequency);
                 }
             }
 
-            foreach(var transfer in feed.GetTransfers())
+            foreach (var transfer in feed.GetTransfers())
             {
-                if(stopIds.Contains(transfer.FromStop.Id) &&
+                if (stopIds.Contains(transfer.FromStop.Id) &&
                     stopIds.Contains(transfer.ToStop.Id))
                 {
                     filteredFeed.AddTransfer(transfer);
