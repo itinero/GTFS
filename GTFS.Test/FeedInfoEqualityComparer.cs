b036ebd854bf209f12352e9f60f061b83c07ceef
Ranko Orlic <ranko.orlic@kapsch.net>
2016-02-10 12:49:55 +0100

    added comments to FeedInfoEqualityComparer

--- GTFS.Test/FeedInfoEqualityComparer.cs
+++ GTFS.Test/FeedInfoEqualityComparer.cs
@@ -8,6 +8,14 @@ namespace GTFS.Test
     /// </summary>
     public sealed class FeedInfoEqualityComparer : IEqualityComparer<FeedInfo>
     {
+        /// <summary>
+        /// Determines whether the specified objects are equal.
+        /// </summary>
+        /// <returns>
+        /// true if the specified objects are equal; otherwise, false.
+        /// </returns>
+        /// <param name="x">The first object of type <see cref="FeedInfo"/> to compare.</param>
+        /// <param name="y">The second object of type <see cref="FeedInfo"/> to compare.</param>
         public bool Equals(FeedInfo x, FeedInfo y)
         {
             if (ReferenceEquals(x, y)) return true;
@@ -15,15 +23,23 @@ namespace GTFS.Test
             if (ReferenceEquals(y, null)) return false;
             if (x.GetType() != y.GetType()) return false;
 
-            return string.Equals(x.PublisherName, y.PublisherName) && 
-                   string.Equals(x.PublisherUrl, y.PublisherUrl) && 
-                   string.Equals(x.Lang, y.Lang) && 
-                   string.Equals(x.StartDate, y.StartDate) && 
-                   string.Equals(x.EndDate, y.EndDate) && 
-                   Equals(x.Tag, y.Tag) && 
+            return string.Equals(x.PublisherName, y.PublisherName) &&
+                   string.Equals(x.PublisherUrl, y.PublisherUrl) &&
+                   string.Equals(x.Lang, y.Lang) &&
+                   string.Equals(x.StartDate, y.StartDate) &&
+                   string.Equals(x.EndDate, y.EndDate) &&
+                   Equals(x.Tag, y.Tag) &&
                    string.Equals(x.Version, y.Version);
         }
 
+        /// <summary>
+        /// Returns a hash code for the specified object.
+        /// </summary>
+        /// <returns>
+        /// A hash code for the specified object.
+        /// </returns>
+        /// <param name="obj">The <see cref="T:System.Object"/> for which a hash code is to be returned.</param>
+        /// <exception cref="T:System.ArgumentNullException">The type of <paramref name="obj"/> is a reference type and <paramref name="obj"/> is null.</exception>
         public int GetHashCode(FeedInfo obj)
         {
             unchecked
