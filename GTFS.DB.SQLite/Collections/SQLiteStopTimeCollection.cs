ed004356dd60aa2d48abb62a91e267d7aa2ec87e
Tim Trewartha <timt@whereismytransport.com>
2016-09-27 11:00:38 +0200

    Delete stop_time by trip_id

--- GTFS.DB.SQLite/Collections/SQLiteStopTimeCollection.cs
+++ GTFS.DB.SQLite/Collections/SQLiteStopTimeCollection.cs
@@ -174,7 +174,18 @@ namespace GTFS.DB.SQLite.Collections
         /// <returns></returns>
         public int RemoveForTrip(string tripId)
         {
-            throw new NotImplementedException();
+            string sql = "DELETE FROM stop_time WHERE FEED_ID = :feed_id AND trip_id = :trip_id;";
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = sql;
+                command.Parameters.Add(new SQLiteParameter(@"feed_id", DbType.Int64));
+                command.Parameters.Add(new SQLiteParameter(@"trip_id", DbType.String));
+
+                command.Parameters[0].Value = _id;
+                command.Parameters[1].Value = tripId;
+
+                return command.ExecuteNonQuery();
+            }
         }
 
         /// <summary>
