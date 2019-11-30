c1d35c2a708a2411864b792a52279a01074cabf3
xivk <ben@itinero.tech>
2019-11-30 09:42:00 +0100

    Fixed Nuget package settings.

--- test/GTFS.Test/Properties/AssemblyInfo.cs
+++ test/GTFS.Test/Properties/AssemblyInfo.cs
@@ -1,19 +1,3 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GTFS.Test")]
-[assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1dc682d7-0ea3-48f3-93f0-735b7d9739fe")]
