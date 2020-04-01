db39b7903e81f5202b286d45cde293c0d830ccfe
Mostafa Satari <m@msatari.ir>
2020-04-01 17:07:35 +0100

    changed filed writers from private to protected since the class is inheritable

--- src/GTFS/GTFSWriter.cs
+++ src/GTFS/GTFSWriter.cs
@@ -811,7 +811,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldExceptionType(string name, string fieldName, ExceptionType value)
+        protected string WriteFieldExceptionType(string name, string fieldName, ExceptionType value)
         {
             //A value of 1 indicates that service has been added for the specified date.
             //A value of 2 indicates that service has been removed for the specified date.
@@ -833,7 +833,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="dateTime"></param>
         /// <returns></returns>
-        private string WriteFieldDate(string name, string fieldName, DateTime dateTime)
+        protected string WriteFieldDate(string name, string fieldName, DateTime dateTime)
         {
             return dateTime.ToString("yyyyMMdd");
         }
@@ -845,7 +845,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldBool(string name, string fieldName, bool? value)
+        protected string WriteFieldBool(string name, string fieldName, bool? value)
         {
             if (value.HasValue)
             {
@@ -865,7 +865,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldUint(string name, string fieldName, uint? value)
+        protected string WriteFieldUint(string name, string fieldName, uint? value)
         {
             if (value.HasValue)
             {
@@ -881,7 +881,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldPaymentMethod(string name, string fieldName, PaymentMethodType value)
+        protected string WriteFieldPaymentMethod(string name, string fieldName, PaymentMethodType value)
         {
             //0 - Fare is paid on board.
             //1 - Fare must be paid before boarding.
@@ -903,7 +903,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldColor(string name, string fieldName, int? value)
+        protected string WriteFieldColor(string name, string fieldName, int? value)
         {
             return value.ToHexColorString();
         }
@@ -924,7 +924,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldDouble(string name, string fieldName, double? value)
+        protected string WriteFieldDouble(string name, string fieldName, double? value)
         {
             if (value.HasValue)
             {
@@ -940,7 +940,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldInt(string name, string fieldName, int? value)
+        protected string WriteFieldInt(string name, string fieldName, int? value)
         {
             if (value.HasValue)
             {
@@ -956,7 +956,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldLocationType(string name, string fieldName, LocationType? value)
+        protected string WriteFieldLocationType(string name, string fieldName, LocationType? value)
         {
             if (value.HasValue)
             {
@@ -978,7 +978,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldDropOffType(string name, string fieldName, DropOffType? value)
+        protected string WriteFieldDropOffType(string name, string fieldName, DropOffType? value)
         {
             if (value.HasValue)
             {
@@ -1001,7 +1001,7 @@ namespace GTFS
         /// Writes the timepoint value.
         /// </summary>
         /// <returns></returns>
-        private string WriteFieldTimepointType(string name, string fieldName, TimePointType value)
+        protected string WriteFieldTimepointType(string name, string fieldName, TimePointType value)
         {
             switch (value)
             {
@@ -1020,7 +1020,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldPickupType(string name, string fieldName, PickupType? value)
+        protected string WriteFieldPickupType(string name, string fieldName, PickupType? value)
         {
             if (value.HasValue)
             {
@@ -1043,7 +1043,7 @@ namespace GTFS
         /// Writes a timeofday.
         /// </summary>
         /// <returns></returns>
-        private string WriteFieldTimeOfDay(string name, string fieldName, TimeOfDay? value)
+        protected string WriteFieldTimeOfDay(string name, string fieldName, TimeOfDay? value)
         {
             if (!value.HasValue)
             {
@@ -1062,7 +1062,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldTransferType(string name, string fieldName, TransferType value)
+        protected string WriteFieldTransferType(string name, string fieldName, TransferType value)
         {
             switch (value)
             {
@@ -1086,7 +1086,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldAccessibilityType(string name, string fieldName, WheelchairAccessibilityType? value)
+        protected string WriteFieldAccessibilityType(string name, string fieldName, WheelchairAccessibilityType? value)
         {
             if (value.HasValue)
             {
@@ -1114,7 +1114,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldIsBidirectional(string name, string fieldName, IsBidirectional? value)
+        protected string WriteFieldIsBidirectional(string name, string fieldName, IsBidirectional? value)
         {
             if (value.HasValue)
             {
@@ -1139,7 +1139,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldPathwayMode(string name, string fieldName, PathwayMode? value)
+        protected string WriteFieldPathwayMode(string name, string fieldName, PathwayMode? value)
         {
             if (value.HasValue)
             {
@@ -1179,7 +1179,7 @@ namespace GTFS
         /// <param name="fieldName"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        private string WriteFieldDirectionType(string name, string fieldName, DirectionType? value)
+        protected string WriteFieldDirectionType(string name, string fieldName, DirectionType? value)
         {
             if (value.HasValue)
             {
